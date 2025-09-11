ceae6617a062d2d64a96b48e0b44df49cdf0dbf2
RamiroSGaspar <gapsar.sebastian@gmail.com>
2025-09-11 20:47:46 -0300

    .

--- TP3_Ejercicio_17.py
+++ TP3_Ejercicio_17.py
@@ -8,8 +8,8 @@ Ej 17
 Mostrar la tabla de multiplicar del 6 (Ej. 6x1=6; 6x2=12; 6x3=18, etc. hasta 6x10)
 '''
 print("--Tabla del 6--")
-n = 0
+n = 6
 for i in range(1,11):
-    n = 6
+    # n = 6
     mult = i * n
     print(f"{n} x {i} = {mult}")
